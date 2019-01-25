7ea2623dd7dc0fe102e2437602f2f180028731ea
sparticvs <sparticvs@popebp.com>
2019-01-24 19:33:14 -0500

    Implement VulnLink #1

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
+/**
+ * Copyright (c) 2019 - Charles `sparticvs` Timko
+ */
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-import styles from './styles.css'
+const BaseUrls = {
+    'nvd': 'https://nvd.nist.gov/vuln/detail/',
+    'mitre': 'http://cve.mitre.org/cgi-bin/cvename.cgi?name='
+};
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
-
-  render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
+export default class VulnLink extends Component {
+    render() {
+        const type = this.props.type in Object.keys(BaseUrls) ? this.props.type : 'nvd';
+        const child = this.props.children == undefined ? this.props.id : this.props.children;
+        return (
+            <a className="vuln-link" href={BaseUrls[type] + this.props.id}>{child}</a>
+        );
+    }
 }
--- src/test.js
+++ src/test.js
@@ -1,7 +1,21 @@
-import ExampleComponent from './'
+import VulnLink from './'
+import React from 'react'
+import TestRenderer from 'react-test-renderer'
 
-describe('ExampleComponent', () => {
+const TEST_CVE = "CVE-2018-16598";
+const TEST_NVD = "https://nvd.nist.gov/vuln/detail/";
+
+describe('VulnLink', () => {
   it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
-  })
+    expect(VulnLink).toBeTruthy()
+  });
+  it('has json match', () => {
+    const testRender = TestRenderer.create(
+        <VulnLink src="nvd" id={TEST_CVE} />
+    );
+    const expectedRender = TestRenderer.create(
+        <a className="vuln-link" href={TEST_NVD + TEST_CVE}>{TEST_CVE}</a>
+    );
+    expect(testRender.toJSON()).toEqual(expectedRender.toJSON());
+  });
 })
