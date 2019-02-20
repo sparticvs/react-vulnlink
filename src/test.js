72bef4279758649827c5169ebf0c0d483305e082
sparticvs <sparticvs@popebp.com>
2019-02-19 21:59:48 -0500

    Update Test for RHSAs #7

--- src/test.js
+++ src/test.js
@@ -3,8 +3,10 @@ import React from 'react'
 import TestRenderer from 'react-test-renderer'
 
 const TEST_CVE = "CVE-2018-16598";
+const TEST_RHSA = "RHSA-2018:0160";
 const TEST_NVD = "https://nvd.nist.gov/vuln/detail/";
 const TEST_MITRE = "https://cve.mitre.org/cgi-bin/cvename.cgi?name=";
+const TEST_REDHAT = "https://access.redhat.com/errata/";
 
 describe('VulnLink', () => {
   it('is truthy', () => {
@@ -25,6 +27,15 @@ describe('VulnLink', () => {
     );
     const expectedRender = TestRenderer.create(
         <a className="vuln-link" href={TEST_MITRE + TEST_CVE}>{TEST_CVE}</a>
+    );
+    expect(testRender.toJSON()).toEqual(expectedRender.toJSON());
+  });
+  it('has json match with RedHat URL', () => {
+    const testRender = TestRenderer.create(
+        <VulnLink src="redhat" id={TEST_RHSA} />
+    );
+    const expectedRender = TestRenderer.create(
+        <a className="vuln-link" href={TEST_REDHAT + TEST_RHSA}>{TEST_RHSA}</a>
     );
     expect(testRender.toJSON()).toEqual(expectedRender.toJSON());
   });
